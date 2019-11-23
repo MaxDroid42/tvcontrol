22e0b09e3451069b52a18f1db4428a7609fac8a1
MaxDroid42 <Maxdroid42@protonmail.com>
2019-11-23 14:46:51 +0100

    init

---
+++ tvcontrol.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Silence will fall! (set time in seconds of silence, before turning of the tv)
+st=3
+
+while true
+do
+    run="$(cat /proc/asound/card*/pcm*/sub*/status | grep -c RUNNING)"
+    if [[ $run != 0 ]]
+    then
+        while [[ "$(echo 'pow 0' | cec-client -s -d 1 | grep -c standby)" != 0 ]]
+        do
+            echo "on 0" | cec-client -s -d 1
+            echo "as" | cec-client -s -d 1
+            sleep 2
+        done
+    else
+        sleep $st
+        run="$(cat /proc/asound/card*/pcm*/sub*/status | grep -c RUNNING)"
+        if [[ $run = 0 ]]
+        then
+            while [[ "$(echo 'pow 0' | cec-client -s -d 1 | grep -c standby)" = 0 ]]
+            do
+                echo "standby 0" | cec-client -s -d 1
+                sleep 2
+            done
+        fi
+    fi
+    sleep 1
+done
